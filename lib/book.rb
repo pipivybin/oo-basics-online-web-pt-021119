5f476c8dd31f715e3a9099a2feb71836c1e9d494
Ming Duan <11203duan@gmail.com>
2019-03-02 18:22:38 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -10,16 +10,4 @@ end
 
 attr_accessor :author, :page_count, :genre, :turn_page
 
-def page_count=(num)
-  @page_count = num
-end
-
-def genre=(sth)
-  @genre = sth
-end
-
-def turn_page
-
-end
-
 end
